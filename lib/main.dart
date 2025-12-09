5f0d94bb0227f4c696a598397c724e67cdd088ce
InnovationLadders <mansour@innovationladders.com>
2025-12-09 15:32:49 +0300

    Updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MyProject Platform',
+      title: 'sky property',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
